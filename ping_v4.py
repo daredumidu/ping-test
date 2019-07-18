87f8f2324dd9c114518ff24305f3f36ed9f6fb3d
Dumidu Darshika Senanayake <52957290+daredumidu@users.noreply.github.com>
2019-07-18 23:06:47 +0530

    Create ping_v4.py

---
+++ ping_v4.py
@@ -0,0 +1,22 @@
+#execute ping and hping3 to hosts listed in a txt file and write the result to a txt file.
+
+import os
+
+f = open ("host.txt", "r")
+f1 = f.readlines()
+
+f = open ("result.txt","w+")
+
+for address in f1:
+ address = address.rstrip() # rstrip - remove the new line
+ response = os.system('ping -c 1 ' + address)
+ 
+ if response == 0:
+  f.write("%s is up \n" % address) # \n - new line, %s - read from %
+ else:
+  response1 = os.system('hping3 -S -p 80 -c 1 ' + address)
+   
+  if response1 == 0:
+   f.write("%s is up \n" % address)
+  else:
+   f.write("%s is down \n" % address)
